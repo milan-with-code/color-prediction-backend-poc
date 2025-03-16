f3a5088921e928b3bd11c1c8e140717d3cdac3a6
milan-with-code <milan.code.github@gmail.com>
2025-03-16 21:32:36 +0530

    server file change

--- server.js
+++ server.js
@@ -16,10 +16,6 @@ const server = http.createServer(app);
 app.use(cors());
 app.use(express.json());
 
-// Basic route
-app.get("/", (req, res) => {
-  res.send("Hello from Color Prediction Game Backend!");
-});
 
 app.use(
   cors({
@@ -29,9 +25,19 @@ app.use(
   })
 );
 
+// Basic route
+app.get("/", (req, res) => {
+  res.send("Hello from Color Prediction Game Backend!");
+});
+
 // Your additional routes would go here (e.g., authentication, game logic, etc.)
 app.use("/api", TimerIdRoutes);
 app.use("/api/auth", authRoutes);
+
+// Start Timer Update Every Second
+setInterval(updateGameTimer, 1000);
+
+// app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
